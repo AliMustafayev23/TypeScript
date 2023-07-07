e0edef7fbd238c7c155b5299960aa2434b6a0e3e
AliMustafayev23 <ali.mustafayev.2002n@gmail.com>
2023-07-07 20:44:23 +0400

    type aliases and interfaces

---
+++ Type Aliases and Interfaces/build/index.js
@@ -0,0 +1,23 @@
+"use strict";
+const carYear = 2001;
+const carType = "Toyota";
+const carModel = "Corolla";
+const car = {
+    year: carYear,
+    type: carType,
+    model: carModel,
+};
+console.log(typeof car.year);
+console.log(typeof car.type);
+console.log(typeof car.model);
+const rectangle = {
+    height: 20,
+    width: 10,
+};
+console.log(rectangle);
+const coloredRectangle = {
+    height: 20,
+    width: 10,
+    color: "red",
+};
+console.log(coloredRectangle);
