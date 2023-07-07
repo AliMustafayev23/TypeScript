63ea3cd46c4545462e21875fcad93ae536c285e0
AliMustafayev23 <ali.mustafayev.2002n@gmail.com>
2023-07-07 18:13:43 +0400

    simple types

---
+++ Simple Types/build/index.js
@@ -0,0 +1,11 @@
+"use strict";
+//Explicit
+let firstName = "Dylan";
+console.log(typeof firstName);
+//Implicit
+let firstName2 = "Dylan";
+console.log(typeof firstName);
+// Implicit any as JSON.parse doesn't know what type of data it returns so it can be "any" thing...
+const json = JSON.parse("55");
+// Most expect json to be an object, but it can be a string or a number like this example
+console.log(typeof json);
