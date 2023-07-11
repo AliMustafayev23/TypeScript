12514132e70ea888adcb844fea4669b0fae910bd
AliMustafayev23 <ali.mustafayev.2002n@gmail.com>
2023-07-11 13:47:21 +0400

    casting

---
+++ Casting/build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let x = "hello";
+console.log(x.length);
+let y = "hello";
+console.log(y.length);
---
+++ Casting/src/index.ts
@@ -0,0 +1,7 @@
+let x: unknown = "hello";
+
+console.log((x as string).length);
+
+let y: unknown = "hello";
+
+console.log((<string>y).length);
